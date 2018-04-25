8fcf6cbf2e99bc7d88264904b62a52bfa8b066c4
Myalicia <My.strandberg@redmind.se>
2018-04-25 14:25:36 +0200

    create user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
 import firebase from 'firebase'
-import {EMAIL_CHANGED,PASSWORD_CHANGED} from './types'
+import {EMAIL_CHANGED,
+        PASSWORD_CHANGED,
+        LOGIN_USER_SUCCESS
+} from './types'
 
 export const emailChanged = (text)=> {
     return{
@@ -17,8 +20,19 @@ export const passwordChanged = (text) => {
 
 export const loginUser = ({email, password}) => {
     return (dispatch) => {
-    firebase.auth().signInWithEmailAndPassword(email,password)
-    .then( user => { dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user}) 
-        })
+        firebase.auth().signInWithEmailAndPassword(email,password)
+        .then( user => { 
+            dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user}) 
+            })
+        .catch( () => {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then( user => {
+            dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user
+            })
+        })    
+    })
+
+
+
     }
 }
