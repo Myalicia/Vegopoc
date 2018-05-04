145f2fb6a5866ac2eb7f95b5618c772f118aa0be
Myalicia <My.strandberg@redmind.se>
2018-05-04 20:02:10 +0200

    employeeform

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,7 +1,29 @@
 import React, {Component} from 'react'
+import { CardSection, Input} from './common'
+import {View, Text} from 'react-native'
 
 class EmployeeForm extends Component {
     render(){
+        return(
+        <View>
+        <CardSection>
+             <Input
+                label="Name"
+                placeholder="sofia"
+                value = {this.props.name}
+                onChangeText = {value => this.props.employeeUpdate({ prop: 'name', value })}
+            />
+        </CardSection>
+        <CardSection>
+            <Input
+                label="Phone"
+                placeholder="070 777 77 77"
+                value={this.props.phone}
+                onChangeText={value => this.props.employeeUpdate({prop: 'phone',value})}
+            />
+        </CardSection>
+        </View>
+       )     
 
     }
 }
